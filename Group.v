b7ee701bf108750956516e99554ed6f4ddcccd80
Solomon Aduol Maina <smaina@seas.upenn.edu>
2019-02-20 18:21:54 -0500

    Trouble defining wreath product

--- Group.v
+++ Group.v
@@ -1,3 +1,6 @@
+Require Import Logic.FunctionalExtensionality.
+Require Import Logic.ProofIrrelevance.
+
 Record Group := {
       G : Type;
       mult : G -> G -> G;
@@ -113,3 +116,110 @@ Proof.
 + apply inverses_exist.
 + apply inverses_exist.
 Defined.
+
+Definition indexed_product_group (X : Group) (I : Type) : Group.
+refine ({|
+        G := I -> (G X);
+        mult f g := (fun i => mult X (f i) (g i));
+        id := (fun i => id X);
+        inverse f := (fun i => inverse X (f i));
+       |}).
+Proof.
+- intros. apply functional_extensionality. intros. apply associative.
+- intros.
+assert ((fun i : I => mult X (id X) (x i)) = (fun i : I => x i)).
+{apply functional_extensionality. intros. apply id_exists. }
+assert ((fun i : I => mult X (x i) (id X)) = (fun i : I => x i)).
+{apply functional_extensionality. intros. apply id_exists. }
+rewrite H. rewrite H0. auto.
+- intros.
+assert ((fun i : I => mult X (x i) (inverse X (x i))) = (fun i : I => id X)).
+{apply functional_extensionality. intros. apply inverses_exist. }
+assert ((fun i : I => mult X (inverse X (x i)) (x i)) = (fun i : I => id X)).
+{apply functional_extensionality. intros. apply inverses_exist. }
+rewrite H. rewrite H0. auto.
+Defined.
+
+Definition bijective A (f : (A -> A) * (A -> A)) :=
+(forall a, (fst f) ((snd f) a) = a /\ (snd f) ((fst f) a) = a).
+
+Fact bijections_compose : forall A (f g : (A -> A) * (A -> A)),
+bijective A f -> bijective A g->
+bijective A ((fun x => (fst g) ((fst f) x)), (fun x => (snd f) ((snd g) x))).
+Proof. intros. unfold bijective in *. destruct f. destruct g. simpl in *.
+simpl. unfold bijective. split.
++ intros. assert (a (a0 (a2 a3)) = a2 a3). {apply H. }
+rewrite H1. apply H0.
++ intros. assert (a2 (a1 (a a3)) = a a3). {apply H0. }
+rewrite H1. apply H.
+Qed.
+
+Fact identity_is_bijective : forall A,
+bijective A ((fun x => x), (fun x => x)).
+Proof. intros. unfold bijective. auto.
+Qed.
+
+Fact inverse_is_bijective : forall A (f : ((A -> A) * (A -> A))),
+bijective A f -> bijective A (snd f, fst f).
+Proof. intros. destruct f. unfold bijective. unfold bijective in H.
+simpl. simpl in *. split; apply H.
+Qed.
+
+Definition permutation_group (A : Type) : Group.
+refine ({|
+        G := {f : ((A -> A) * (A -> A)) | bijective A f};
+        mult m n := let (f, p) := m in
+                    let (g, p') := n in
+                    exist (bijective A) 
+                    ((fun x => (fst g) ((fst f) x)), 
+                    (fun x => (snd f) ((snd g) x)))
+                    (bijections_compose A f g p p');
+        id := exist (bijective A) ((fun x => x), (fun x => x))
+             (identity_is_bijective A);
+        inverse m := let (f, p) := m in 
+                     exist (bijective A) (snd f, fst f) 
+                     (inverse_is_bijective A f p);
+       |}).
+Proof.
+- intros. destruct x. destruct y. destruct z. destruct x.
+destruct x0. destruct x1. simpl in *. simpl.
+apply subset_eq_compat. auto.
+- intros. destruct x. destruct x. simpl. split.
++ apply subset_eq_compat. auto.
++ apply subset_eq_compat. auto.
+- intros. destruct x. destruct x. simpl. split.
++ apply subset_eq_compat. unfold bijective in b. simpl in *.
+assert ((fun x : A => a0 (a x) ) = (fun x : A => x) ).
+{apply functional_extensionality. intros. apply b. }
+rewrite H. auto.
++ apply subset_eq_compat. unfold bijective in b. simpl in *.
+assert ((fun x : A => a (a0 x) ) = (fun x : A => x) ).
+{apply functional_extensionality. intros. apply b. }
+rewrite H. auto.
+Defined.
+
+Definition wreath_product (X : Group) : Group.
+refine ({|
+        G := (G (indexed_product_group X nat)) * (G (permutation_group nat));
+        mult m n := let (gs, bij) := m in
+                    let (f, p) := bij in 
+                    let (pi, _) := f in
+                    let (gs', bij') := n in
+                    let (f', p') := bij' in 
+                    let (pi', _) := f' in
+                    let new_gs := (fun i => mult _ (gs' (pi i)) (gs i) ) in
+                    let new_bij := exist _
+                    ((fun x => (fst f') ((fst f) x)), 
+                    (fun x => (snd f) ((snd f') x)))
+                    (bijections_compose _ f f' p p') in (new_gs, new_bij);
+        id := ((fun _ => id X), exist _ ((fun n => n), (fun n => n)) (identity_is_bijective _));
+        inverse m := let (gs, bij) := m in
+                    let (f, p) := bij in 
+                    let (pi, pi_inv) := f in
+                    let new_bij := inverse _ bij in 
+                    let new_f := (fun n => inverse _ (gs (pi_inv n))) in
+                    (new_f, new_bij)
+       |}).
+Proof.
+- intros. destruct x. destruct y. destruct z. destruct g0. destruct x.
+destruct g2. destruct x. destruct g4. destruct x. simpl.
